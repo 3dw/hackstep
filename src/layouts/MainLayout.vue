51136546a1e90327fd8f829bad5a5cc7bda7fb11
Bestian Tang 大巴 <bestian@gmail.com>
2024-03-17 18:56:13 +0800

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,10 +3,13 @@
 q-layout(view="lHh Lpr lFf")
   q-header(elevated)
     q-toolbar
-      // 定義二個按鈕：編輯步驟和複製連結
       q-btn(flat, dense, round, icon="menu", aria-label="Menu", @click="toggleLeftDrawer")
-      //q-btn(@click="editStep" flat round icon="edit" aria-label="Edit Step")
       q-btn(@click="copyLink" flat round icon="ios_share" aria-label="Share" label="Share")
+      q-btn(@click="downloadSteps" flat round icon="cloud_download" aria-label="Download")
+      q-btn(flat round icon="cloud_upload" @click="clickFileUpload()")
+      input(type="file" accept=".md" @change="handleFileUpload" ref="fileInput" style="display: none;")
+
+
   q-drawer(v-model="leftDrawerOpen", show-if-above, bordered)
     q-list
       q-item 
@@ -68,6 +71,44 @@ export default defineComponent({
       this.$router.push(
         '/edit/_' + this.hash.replace('edit/', '').replace('_/', '')
       );
+    },
+    downloadSteps() {
+      const title = window.prompt('Please Enter a Title:');
+      const steps = this.$route.params.steps.split(/%20|\s/)
+      let markdownContent = steps.map((step, index) => `${index + 1}. ${step}`).join('\n');
+      // 添加文件開頭，如有需要
+      markdownContent = '# ' + title + '\n\n' + markdownContent;
+      
+      const blob = new Blob([markdownContent], { type: 'text/markdown;charset=utf-8' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = title + '.md';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    },
+    clickFileUpload() {
+      this.$refs.fileInput.click(); // 觸發檔案選擇
+    },
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const content = e.target.result;
+          const steps = this.parseMarkdown(content);
+          const stepsJoined = steps.join(' ');
+          this.$router.push({ path: `/edit/${encodeURIComponent(stepsJoined)}` });
+          // bug here..... ///
+
+        };
+        reader.readAsText(file);
+      }
+    },
+    parseMarkdown(content) {
+      const lines = content.split('\n');
+      const steps = lines.filter(line => line.match(/^\d+\.\s+/)).map(line => line.replace(/^\d+\.\s+/, ''));
+      return steps; // 返回解析後的步驟列表
     },
     copyLink() {
       // 複製當前頁面的連結到剪貼簿
