ed5ef30ca8b246b19fdbc536b969ab361324dba3
Bestian Tang <bestian@gmail.com>
2024-04-04 20:12:04 +0800

    build -m pwa 2024-04-04

--- dist/pwa/assets/i18n.e8c97a45.js
+++ dist/pwa/assets/i18n.e8c97a45.js
@@ -1,4 +1,4 @@
-import{d as ct,h as Ht,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Xt,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.da2ec900.js";/*!
+import{d as ct,h as Ht,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Xt,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.a875f4a0.js";/*!
   * shared v9.3.0
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
